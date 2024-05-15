06fbe8050970b91521e475b9b9da1335b3e79c24
jhn6666 <12210308@mail.sustech.edu.cn>
2024-05-16 02:54:12 +0800

    My first decorator function

--- Decorators/First decorator.py
+++ Decorators/First decorator.py
@@ -9,14 +9,14 @@ def a_function_requiring_decoration():
     print("I am the function which needs some decoration to remove my foul smell")
    
    
-print("#############################################")    
-a_function_requiring_decoration()
+# print("#############################################")    
+# a_function_requiring_decoration()
 #outputs: "I am the function which needs some decoration to remove my foul smell
 
 a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
 
-print("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
-a_function_requiring_decoration()
+# print("LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
+# a_function_requiring_decoration()
 """ 
 Outputs:
 I am doing some boring work before executing a_func()
@@ -24,3 +24,40 @@ I am the function which needs some decoration to remove my foul smell
 I am doing some boring work after executing a_func()
 """
 
+@ a_new_decorator
+def a_function_requiring_decoration():
+    print("I am the function which needs some decoration to remove my foul smell")
+
+a_function_requiring_decoration()
+
+""" 
+I am doing some boring work before executing a_func()
+I am the function which needs some decoration to remove my foul smell
+I am doing some boring work after executing a_func()
+"""
+
+print("#############################################")
+print(a_function_requiring_decoration.__name__)
+""""
+输出结果是wrapTheFunction，这并不是我们想要的。因为我们的函数被wrapTheFunction替代了
+我们可以使用functools.wraps来解决这个问题
+"""
+
+
+from functools import wraps
+
+def a_new_decorator_using_wraps(a_func):
+    @wraps(a_func)
+    def wrapTheFunction():
+        print("I am doing some boring work before executing a_func()")
+        a_func()
+        print("I am doing some boring work after executing a_func()")
+    return wrapTheFunction
+
+@a_new_decorator_using_wraps
+def a_function_requiring_decoration():
+    print("I am the function which needs some decoration to remove my foul smell")
+
+print("#############################################")
+print(a_function_requiring_decoration.__name__)
+#输出结果是a_function_requiring_decoration，这就是我们想要的结果
